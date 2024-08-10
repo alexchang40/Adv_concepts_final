29ddad0f4e78a252c053f94749eb2226fe021bfb
Alex Chang <33704195+alexchang40@users.noreply.github.com>
2024-08-10 13:53:55 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,4 +9,9 @@ test_seq4 = "APLSYVQLKDIAELNKEKRHAIQQAVNHTPGGYDPL"
 test_seq5 = "EKRNDQIAAQKFAKVQHYRGVNAKRDQFAKLIAEVDTKEFHRY"
 
 protein1 = protein.Protein(test_seq1)
+protein2 = protein.Protein(test_seq2)
 print(protein1.aa_count)
+print(protein1.weight)
+print(protein1.hydropathy)
+print(protein1.extinction)
+print(protein1.pH_74)
