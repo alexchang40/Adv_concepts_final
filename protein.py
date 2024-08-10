4f62771f7a6ed024b578ebafb02d15d22f4b0a2c
Alex Chang <33704195+alexchang40@users.noreply.github.com>
2024-08-10 13:45:45 -0700

    Update protein.py

--- protein.py
+++ protein.py
@@ -6,6 +6,10 @@ class Protein:
   def __init__(self, sequence):
     self.sequence = sequence
     self.count_amino_acids()
+    self.weight = self.get_molecular_property("weight")
+    self.hydropathy = self.get_molecular_property("hydropathy")
+    self.extinction = self.get_molecular_property("extinction")
+    self.pH_7.4 = self.get_molecular_property(7.4)
 
   #count the numbers of each amino acid in the sequence
   def count_amino_acids(self):
@@ -19,3 +23,18 @@ class Protein:
       else:
         aa_count[amino_acid] = 1
     self.aa_count = aa_count
+
+  #calculate a specific molecular property for the protein
+  def get_molecular_property(desired_property):
+    if desired_property == "weight":
+      return "It returned weight"
+    elif desired_property == "hydropathy":
+      return "It returned hydropathy"
+    elif desired_property == "extinction":
+      return "It returned extinction"
+    #in this case desired property is pH
+    else:
+      return "It returned the pH " + desired_property
+    
+
+    
