c19a09118fca993a4e52cdd7519d88f2a638affc
Alex Chang <33704195+alexchang40@users.noreply.github.com>
2024-08-10 18:42:25 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,11 +9,11 @@ test_seq4 = "APLSYVQLKDIAELNKEKRHAIQQAVNHTPGGYDPL"
 test_seq5 = "EKRNDQIAAQKFAKVQHYRGVNAKRDQFAKLIAEVDTKEFHRY"
 
 protein1 = protein.Protein(test_seq1)
-protein2 = protein.Protein(test_seq2)
+
 print(protein1.sequence)
-#print(protein1.aa_count)
-#print(protein1.weight)
-#print(protein1.hydropathy)
-#print(protein1.extinction)
-print(protein1.charge_at_74)
+print(protein1.aa_count)
+print(protein1.weight)
+print(protein1.hydropathy)
+print(protein1.extinction)
+print(protein1.charge_74)
 print(protein1.pI)
