2eae1c88d80ba82017959ae47d8fb6c568b40551
Alex Chang <33704195+alexchang40@users.noreply.github.com>
2024-08-10 19:24:32 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,9 +6,9 @@ test_seq1 = "MAVNKYRGDILQEYALQKST"
 test_seq2 = "LQNTKTYRFLQRKNEPSIYDDVNRW"
 test_seq3 = "GSGKTNVQLIRLQKYTGTQVKIAERLKNFA"
 test_seq4 = "APLSYVQLKDIAELNKEKRHAIQQAVNHTPGGYDPL"
-test_seq5 = "EKRNDQIAAQKFAKVQHYRGVNAKRDQFAKLIAEVDTKEFHRY"
+test_seq5 = "EKRNDQIAAQKFAKVQHYRGVNAKRDQFAKLIAEVDTKEFHRYBB"
 
-protein1 = protein.Protein(test_seq1)
+protein1 = protein.Protein(test_seq5)
 
 print(protein1.sequence)
 print(protein1.aa_count)
