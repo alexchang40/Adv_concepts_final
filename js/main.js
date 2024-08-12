a46cc8b4fc538bf64a71d66251b10949db908374
Alex Chang <33704195+alexchang40@users.noreply.github.com>
2024-08-11 18:17:14 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,11 +20,11 @@ function sendSequence() {
 
 function processJSON( data ) {
     $("#results").append("<p><strong>Sequence:</strong> " + data.sequence + "</p>");
-    $("#results").append("<p><strong>Length:</strong> " + data.length + "</p>");
-    $("#results").append("<p><strong>Weight:</strong> " + data.weight + "</p>");
+    $("#results").append("<p><strong>Length:</strong> " + data.length + " amino acids</p>");
+    $("#results").append("<p><strong>Weight:</strong> " + data.weight + " daltons</p>");
     $("#results").append("<p><strong>Hydropathy:</strong> " + data.hydropathy + "</p>");
-    $("#results").append("<p><strong>Extinction Coefficient:</strong> " + data.extinction + "</p>");
-    $("#results").append("<p><strong>Charge at pH 7.4:</strong> " + data.charge + "</p>");
+    $("#results").append("<p><strong>Extinction Coefficient:</strong> " + data.extinction + "M-1cm-1</p>");
+    $("#results").append("<p><strong>Charge at pH 7.4:</strong> " + data.pH_74 + "</p>");
     $("#results").append("<p><strong>pI:</strong> " + data.pI + "</p>");
     $("#results").show();
 }
